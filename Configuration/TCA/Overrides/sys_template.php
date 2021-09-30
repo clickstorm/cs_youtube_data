58393607687091934fd52232bebd4040abf30a7d
akoenig <koenig@clickstorm.de>
2021-09-30 13:42:16 +0200

    [TASK] move ext_tables functions into TCA Overrides

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,11 @@
+<?php
+
+defined('TYPO3_MODE') or die();
+
+/* Default TS */
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'cs_youtube_data',
+    'Configuration/TypoScript',
+    'Youtube Data'
+);
+
---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,19 @@
+<?php
+
+defined('TYPO3_MODE') or die();
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'Clickstorm.' . 'cs_youtube_data',
+    'Pi1',
+    'Youtube Data V3 Plugin'
+);
+
+//FlexForm add
+$pluginSignature = str_replace('_', '', 'cs_youtube_data') . '_pi1';
+$TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+$TCA['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature]='code,layout,select_key,pages,recursive';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+    $pluginSignature,
+    'FILE:EXT:cs_youtube_data/Configuration/FlexForms/flexform_pi1.xml'
+);
+
